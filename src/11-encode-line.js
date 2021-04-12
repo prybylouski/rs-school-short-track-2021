f39dfa69dd4945ef9bb322795d193dfdf8c55df4
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-04-13 00:20:24 +0300

    refactor: solve 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  // throw new Error('Not implemented');
+  let codeNum = 1;
+  let encodeStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      codeNum++;
+    } else if (codeNum === 1) {
+      encodeStr += str[i];
+    } else {
+      encodeStr += `${codeNum}${str[i]}`;
+      codeNum = 1;
+    }
+  }
+  return encodeStr;
 }
 
 module.exports = encodeLine;
