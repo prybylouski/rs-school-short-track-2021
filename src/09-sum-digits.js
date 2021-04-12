c1d6e1681d3aee814a7eba8e0b25eefa1aa80058
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-04-12 23:49:46 +0300

    refactor: solve next task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,15 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  // throw new Error('Not implemented');
+  if (n === 0) {
+    return 0;
+  }
+  if (n % 9 === 0) {
+    return 9;
+  }
+  return n % 9;
 }
 
 module.exports = getSumOfDigits;
--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,16 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  // throw new Error('Not implemented');
+  let counter = 0;
+  for (let i = array.length; i >= 0; i -= 1) {
+    if (value === array[i]) {
+      counter = i;
+      return counter;
+    }
+  }
+  return counter;
 }
 
 module.exports = findIndex;
